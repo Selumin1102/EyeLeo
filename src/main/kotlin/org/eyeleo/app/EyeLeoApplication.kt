4eaa050fce2e6f5f0e5297a81a104959a5d2e8bc
Selyumin <Selyumin.V.Al@sberbank.ru>
2020-08-24 14:09:20 +0300

    Project created.

---
+++ src/main/kotlin/org/eyeleo/app/EyeLeoApplication.kt
@@ -0,0 +1,11 @@
+package org.eyeleo.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EyeLeoApplication
+
+fun main() {
+    runApplication<EyeLeoApplication>()
+}
---
+++ src/test/kotlin/org/eyeleo/app/EyeLeoApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.eyeleo.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EyeLeoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
