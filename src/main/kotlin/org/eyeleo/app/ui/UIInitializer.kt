b8d5cddd09f98b61542b77fd4b4140f85e14a8aa
Selyumin <Selyumin.V.Al@sberbank.ru>
2020-08-30 23:17:22 +0300

    Try to build UI.

--- src/main/kotlin/org/eyeleo/app/ui/UIInitializer.kt
+++ src/main/kotlin/org/eyeleo/app/ui/UIInitializer.kt
@@ -1,9 +1,5 @@
 package org.eyeleo.app.ui
 
-import javafx.geometry.Pos
-import javafx.scene.Node
-import javafx.scene.control.Label
-import javafx.scene.layout.VBox
 import org.eyeleo.app.ui.tray.TrayInitializer
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
@@ -18,28 +14,5 @@ class UIInitializer(
         val stage = event.getStage()
 
         SwingUtilities.invokeLater { trayInitializer.initTray(UIInitializationContext(stage)) }
-
-//        stage.initStyle(StageStyle.TRANSPARENT)
-//
-//        val layout = StackPane(createContent())
-//        layout.style = "-fx-background-color: rgba(255, 255, 255, 0.5);"
-//        layout.setPrefSize(300.0, 200.0)
-//
-//        layout.onMouseClicked = EventHandler { stage.hide() }
-//
-//        val scene = Scene(layout)
-//        scene.fill = Color.TRANSPARENT
-//
-//        stage.scene = scene
-    }
-
-    private fun createContent(): Node {
-        val hello = Label("hello, world")
-        hello.style = "-fx-font-size: 40px; -fx-text-fill: #228b22;"
-        val instructions = Label("(click to hide)")
-        instructions.style = "-fx-font-size: 12px; -fx-text-fill: orange;"
-        val content = VBox(10.0, hello, instructions)
-        content.alignment = Pos.CENTER
-        return content
     }
 }
